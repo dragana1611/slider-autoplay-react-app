689bdbdd47304a3755f481f98dfcf9e331389c88
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-02-23 19:34:27 +0100

    aded alternative js

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ function App() {
       setIndex(index + 1);
     }, 3000);
 
-return () => clearInterval(slider)
-
+    return () => clearInterval(slider);
   }, [index]);
 
   return (
@@ -68,5 +67,169 @@ return () => clearInterval(slider)
     </section>
   );
 }
+
+/*  
+Alternative 1
+////////////////////
+
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = React.useState(0);
+
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      const result = (oldIndex + 1) % people.length;
+      return result;
+    });
+  };
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      const result = (oldIndex - 1 + people.length) % people.length;
+      return result;
+    });
+  };
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        const result = (oldIndex + 1) % people.length;
+        return result;
+      });
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = 'nextSlide';
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide';
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          );
+        })}
+        <button className='prev' onClick={prevSlide}>
+          <FiChevronLeft />
+        </button>
+        <button className='next' onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+
+
+Alternative 2 
+////////////////////////////
+
+function App() {
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = React.useState(0)
+
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1
+      if (index > people.length - 1) {
+        index = 0
+      }
+      return index
+    })
+  }
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1
+      if (index < 0) {
+        index = people.length - 1
+      }
+      return index
+    })
+  }
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1
+        if (index > people.length - 1) {
+          index = 0
+        }
+        return index
+      })
+    }, 5000)
+    return () => {
+      clearInterval(slider)
+    }
+  }, [index])
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className='section-center'>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person
+
+          let position = 'nextSlide'
+          if (personIndex === index) {
+            position = 'activeSlide'
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = 'lastSlide'
+          }
+
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className='person-img' />
+              <h4>{name}</h4>
+              <p className='title'>{title}</p>
+              <p className='text'>{quote}</p>
+              <FaQuoteRight className='icon' />
+            </article>
+          )
+        })}
+        <button className='prev' onClick={prevSlide}>
+          <FiChevronLeft />
+        </button>
+        <button className='next' onClick={nextSlide}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  )
+}
+
+*/
 
 export default App;
